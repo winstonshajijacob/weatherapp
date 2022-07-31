605dca2c51400524845da5b495b21d253368a191
Winston Shaji Jacob <winstonshajijacob@gmail.com>
2022-07-31 10:12:32 -0400

    Implemented the logic for organizing data from API calls and displaying them on the template

---
+++ weather_env/weather/weather_conditions.py
@@ -0,0 +1,39 @@
+import weather.views
+from django.shortcuts import redirect
+from django.shortcuts import render
+
+
+def cloud(request):
+    context = weather.views.sheetPandas('Clouds')
+    return render(request, 'weather/index.html', context)
+    # return redirect(weather.views.sheetSync(request,'Clouds'))
+
+def thunderstorm(request):
+    context = weather.views.sheetPandas('Thunderstorm')
+    return render(request, 'weather/index.html', context)
+    # return redirect(weather.views.sheetSync(request,'Thunderstorm'))
+
+def drizzle(request):
+    context = weather.views.sheetPandas('Drizzle')
+    return render(request, 'weather/index.html', context)
+    # return redirect(weather.views.sheetSync(request,'Drizzle'))
+
+def rain(request):
+    context = weather.views.sheetPandas('Rain')
+    return render(request, 'weather/index.html', context)
+    # return redirect(weather.views.sheetSync(request,'Rain'))
+
+def snow(request):
+    context = weather.views.sheetPandas('Snow')
+    return render(request, 'weather/index.html', context)
+    # return redirect(weather.views.sheetSync(request,'Snow'))
+
+def clear(request):
+    context = weather.views.sheetPandas('Clear')
+    return render(request, 'weather/index.html', context)
+    # return redirect(weather.views.sheetSync(request,'Clear'))
+
+def atmosphere(request):
+    context = weather.views.sheetPandas('Atmosphere')
+    return render(request, 'weather/index.html', context)
+    # return redirect(weather.views.sheetSync(request,'Atmosphere'))
